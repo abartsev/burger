1cb07f17c28639e1415cca6d5c904026f5eb45d3
Барцев Анатолий Владимирович <17465388@sigma.sbrf.ru>
2019-05-16 14:14:37 +0300

    sass

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [
+            // fallback to style-loader in development
+            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+        ]
+    }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
